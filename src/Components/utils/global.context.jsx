b7f297dc6746f4afb27800d2f83a47fa93499d36
delfinaferaud <delfinaferaud@gmail.com>
2023-04-03 12:22:10 -0300

    commit

--- src/Components/utils/global.context.jsx
+++ src/Components/utils/global.context.jsx
@@ -6,6 +6,7 @@ export const ContextGlobal = createContext(undefined);
 
 export const ContextProvider = ({ children }) => {
   //Aqui deberan implementar la logica propia del Context, utilizando el hook useMemo
+  //sjdsakjdsa
 
   return (
     <ContextGlobal.Provider value={{}}>
